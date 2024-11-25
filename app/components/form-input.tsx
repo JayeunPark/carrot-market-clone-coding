9688485d77ae530d4f8271979712ef20749ed3f6
Jayeun Park <jayeunpakr0704@gmail.com>
2024-11-25 16:26:07 +0900

    Create Form Components

---
+++ app/components/form-input.tsx
@@ -0,0 +1,30 @@
+// 따로 이유가 없다면 type보다 interface사용을 권장함(ts공식)
+interface FormInputProps {
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+}
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+}: FormInputProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1
+             focus:ring-4 transition ring-neutral-500 focus:ring-orange-500 border-none placeholder:text-neutral-400"
+        type={type}
+        placeholder={placeholder}
+        required={required}
+      />
+      {errors.map((error, index) => (
+        <span key={index} className="text-red-500 font-medium">
+          {error}
+        </span>
+      ))}
+    </div>
+  );
+}
