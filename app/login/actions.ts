9bb51133164830da9445ac87a83b77ef2ac1f0bd
Jayeun Park <jayeunpakr0704@gmail.com>
2024-11-27 14:20:44 +0900

    Validate login

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -2,14 +2,37 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { z } from "zod";
+import {
+  PASSWORD_MIN_LENGTH,
+  PASSWORD_REGEX,
+  PASSWORD_REGEX_ERROR,
+} from "../lib/constants";
 
-export async function handleForm(prevState: any, formData: FormData) {
-  console.log(prevState);
-  await new Promise((resolve) => setTimeout(resolve, 5000));
-  // console.log(formData.get("email"), formData.get("password"));
-  // console.log("logged in!");
+const formSchema = z.object({
+  email: z.string().email().toLowerCase(),
+  password: z
+    .string({
+      required_error: "Password is required",
+    })
+    .min(PASSWORD_MIN_LENGTH)
+    .regex(PASSWORD_REGEX, PASSWORD_REGEX_ERROR),
+});
 
-  return {
-    errors: ["wrong password", "password too short"],
+export async function login(prevState: any, formData: FormData) {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
+  const data = {
+    email: formData.get("email"),
+    password: formData.get("password"),
   };
+  const result = formSchema.safeParse(data);
+  if (!result.success) {
+    console.log(result.error.flatten());
+    return result.error.flatten();
+  } else {
+    console.log(result.data);
+  }
+  // return {
+  //   errors: ["wrong password", "password too short"],
+  // };
 }
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,16 +1,16 @@
 "use client";
-import FormInput from "../components/input";
-import FormButton from "../components/button";
+import Input from "../components/input";
+import Button from "../components/button";
 import SocialLogin from "../components/social-login";
-import { useFormState } from "react-dom";
-import { handleForm } from "./actions";
+import { login } from "./actions";
 import { useActionState } from "react";
+import { PASSWORD_MIN_LENGTH } from "../lib/constants";
 
 export default function Login() {
   // [form에서 돌려줄 return value, form을 클릭했을 때 실행할 액션] = useFormState(실행할action,초기값)
   // useFormState는 handleForm 뿐 아니라 이전에 반환한 state와 함께 실행될 것
   //  useFormState > useActionState(이름바뀜)
-  const [state, action] = useActionState(handleForm, null);
+  const [state, dispatch] = useActionState(login, null);
 
   // form이 로딩되고 있다는 사실을 사용자들에게 알려주기 위한 hook
   // 액션을 실행하는 곳에서 사용할 수 없는 hook임. form의 자식(내부)에서만 사용가능함.
@@ -21,15 +21,23 @@ export default function Login() {
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">Log in with email and password.</h2>
       </div>
-      <form action={action} className="flex flex-col gap-3">
-        <FormInput name="email" type="email" placeholder="Email" required />
-        <FormInput
+      <form action={dispatch} className="flex flex-col gap-3">
+        <Input
+          name="email"
+          type="email"
+          placeholder="Email"
+          required
+          errors={state?.fieldErrors.email}
+        />
+        <Input
           name="password"
           type="password"
           placeholder="Password"
           required
+          minLength={PASSWORD_MIN_LENGTH}
+          errors={state?.fieldErrors.password}
         />
-        <FormButton text="Log in" />
+        <Button text="Log in" />
       </form>
       <SocialLogin />
     </div>
