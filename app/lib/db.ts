64f4ebe95e1e68af0d1522a8b4f487673c501c61
Jayeun Park <jayeunpakr0704@gmail.com>
2024-11-28 19:47:37 +0900

    Create Prisma Client

---
+++ app/lib/db.ts
@@ -0,0 +1,14 @@
+import { PrismaClient } from "@prisma/client";
+
+const db = new PrismaClient();
+
+// async function test() {
+//   const user = await db.user.create({
+//     data: {
+//       username: "asdf",
+//     },
+//   });
+//   console.log(user);
+// }
+// test();
+export default db;
